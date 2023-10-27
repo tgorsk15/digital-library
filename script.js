7f278c1e2feab17fb16f0dc47e6e3ea5310c9683
tgorsk15 <tgbusiness463@gmail.com>
2023-10-27 08:25:35 -0500

    Link form input elements to JS variables
    Linked the values that the user submits using the <form> to functionality in JS. However it is
    still to be determined if it is taking the actual values of 'bookName', etc.. instead of just
    the object itself

--- script.js
+++ script.js
@@ -6,19 +6,38 @@ function Book(name, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    // put 
 }
 
 
 // adding the contents and styling of the card
 // to the bookshelf.
 // this is where name, author, etc info is used...
-function addBookToLibrary() {
-
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.log(myLibrary)
 };
 
+
+
 // submit button that triggers a creation of an
-// object base don the Book constructor
+// object based on the Book constructor
 const addBook = document.querySelector('.add-book')
-addBook.addEventListener('click', () => {
-    
+addBook.addEventListener('click', (event) => {
+    event.preventDefault();
+    // when pressed, book must be added to 'myLibrary'
+    // a new object based on Book() must be created
+    const bookName = document.getElementById('book-name');
+    const authorName = document.getElementById('author-name');
+    const pagesNumber = document.getElementById('pages-number');
+    const readBook = document.getElementById('read-book');
+
+    // creates the new object with Book() as the prototype
+    const book = new Book(bookName, authorName, pagesNumber,
+        readBook)
+    console.log(book)
+
+    addBookToLibrary(book)
 });
+
+console.log('test')
