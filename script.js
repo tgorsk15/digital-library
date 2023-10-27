e0385c3ca4e25f6b39f173ecd7f0012b334b983c
tgorsk15 <tgbusiness463@gmail.com>
2023-10-27 13:14:27 -0500

    Create bookCard(s) each instance of book
    Was able to successfully link up the values passed through the form to what is displayed
    in the card that appears in the bookshelf on the left. Each instance of 'book' should appear
    below the most recent card, thus creating a ever growing list of books in the library

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // array to store only the book OBJECTS
 const myLibrary = [];
 
-function Book(name, author, pages, isRead) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+function Book(bookName, authorName, pagesNumber, readBook) {
+    this.bookName = bookName;
+    this.authorName = authorName;
+    this.pagesNumber = pagesNumber;
+    this.readBook = readBook;
     // put 
 }
+
+
+// reference to cards-holder section
+const cardStorage = document.querySelector('.cards-holder')
+
+Book.prototype.createCard = function() {
+    console.log('test')
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    // problem: Coming up as 'undefined' for the below
+    
+    bookCard.textContent = `This is a test for ${bookName.value}`;
+    cardStorage.appendChild(bookCard);
+};
 
 
 function addBookToLibrary(book) {
@@ -18,38 +32,28 @@ function addBookToLibrary(book) {
 // reference to add book form
 const bookForm = document.getElementById('book-form');
 
-
+const bookName = document.getElementById('book-name');
+const authorName = document.getElementById('author-name');
+const pagesNumber = document.getElementById('pages-number');
+const readBook = document.getElementById('read-book');
 // submit button that triggers a creation of an
 // object based on the Book constructor
 const addBook = document.querySelector('.add-book')
 addBook.addEventListener('click', (event) => {
     event.preventDefault();
-    // when pressed, book must be added to 'myLibrary'
-    // a new object based on Book() must be created
-    const bookName = document.getElementById('book-name');
-    const authorName = document.getElementById('author-name');
-    const pagesNumber = document.getElementById('pages-number');
-    const readBook = document.getElementById('read-book');
     
     // creates the new object with Book() as the prototype
     const book = new Book(bookName.value, authorName.value, pagesNumber.value,
         readBook.value)
     console.log(book)
 
+    // adds the instance of book to myLibrary
     addBookToLibrary(book);
-    createCard(book);
+    
+    book.createCard();
+
     bookForm.reset();
 });
 
-// reference to cards-holder section
-const cardStorage = document.querySelector('.cards-holder')
-
-function createCard(book) {
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    // problem: Coming up as 'undefined' for the below
-    bookCard.textContent = `This is a test for ${book.bookName}`;
-    cardStorage.appendChild(bookCard);
-}
 
 console.log('test')
